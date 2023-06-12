a739bc5abbbcfa3f7e30f5670a5bfb4537b20326
Russell Hedrick <hedrickrussell@gmail.com>
2023-06-11 21:35:07 -0700

    Update water feed script

--- sensor_programs/gardano_control.py
+++ sensor_programs/gardano_control.py
@@ -16,24 +16,45 @@ def motor_off(pin):
     GPIO.output(pin, GPIO.HIGH)
     GPIO.cleanup()
 
-def read_ai(channel):
+def read_ai(chan):
 
-    adc = MCP3008(channel=channel, device=0)
-    return adc.value
+    adc = MCP3008(channel=chan)
+    output = adc.value
+    voltage = round(3.3 * output, 1)
+    return voltage
+
+def vegetronix_moist(v):
+
+    if v <= 1.1:
+        vwc = 10*v - 1
+    elif v > 1.1 and v <= 1.3:
+        vwc = 25*v - 17.5
+    elif v > 1.3 and v <= 1.82:
+        vwc = 48.08*v - 47.5
+    elif v > 1.82 and v <= 2.2:
+        vwc = 26.32*v - 7.89
+    else:
+        vwc = 62.5*v - 87.5
+
+    return round(vwc, 1)
+
 
 if __name__ == '__main__':
     motor1 = 17
     motor2 = 18
     try:
+        temp = (read_ai(0) * 75) - 40
+        moisture = vegetronix_moist(read_ai(3))
+        print(temp)
+        print(moisture)
+
         time_on = int(input("Enter desired pump run time:\n"))
         motor_on(motor1)
         motor_on(motor2)
         time.sleep(time_on)
         motor_off(motor1)
         motor_off(motor2)
-        val = read_ai(0)
-        print(val)
-
+        
     except KeyboardInterrupt:
         print("\nProgram terminated as requested.")
 
