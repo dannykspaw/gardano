3b07750ac752c1310442036f03928731bed2f88b
Russell Hedrick <hedrickrussell@gmail.com>
2023-06-17 21:12:48 -0500

    Remove old sensor script

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ Description:
 #import climate_shit.random_function as rf
 import utils
 
+try:
+    pass
+except KeyboardInterrupt:
+    print("\nProgram terminated as requested.")
+
+except Exception as e:
+    print("\nAn unkown error occured at {}:{}. Program terminated.".format(datetime.now().hour, datetime.now().minute))
+    print(e)
 
 utils.get_time()
 
