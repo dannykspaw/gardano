240ce91546bc2ad23838055eab625de0e1965bf1
dannykspaw <dannykspaw@gmail.com>
2023-06-18 21:51:32 -0500

    pushing up logger script

--- climate_api/logger.py
+++ climate_api/logger.py
@@ -1,20 +1,24 @@
 from weather_data import WeatherData
 import pandas as pd
 
+"""Specify latitude and logitude"""
 latitude = 30.2672
 longitude = -97.7431
 
+"""Call weather API"""
 weather_api = WeatherData(latitude, longitude)
 location = weather_api.location
 weather_data = weather_api.get_weather()
 
-print(weather_data)
+"""Store weather API data and future sensor data as a simple dict can just add sensor inputs here """
+weather_info = {
+'Temperature': [float(weather_data[0])]
+,'Dewpoint': [float(weather_data[1])]
+,'Relative Humidity': [float(weather_data[2])]
+}
 
-weather_info_store = pd.DataFrame()
-weather_info_store['Temperature'] = weather_data[0]
-weather_info_store['Dewpoint'] = weather_data[1]
-weather_info_store['Relative Humidity'] = weather_data[2]
+weather_info_store = pd.DataFrame.from_dict(weather_info)
 
-print(weather_info_store)
+# weather_info_store.to_csv('')
 
 
